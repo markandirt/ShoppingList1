1f40d90fca46324c889be568d2ffe0b061d5cebb
markandirt <markandirt@gmail.com>
2017-10-27 17:32:31 +0200

    ShoppingList 3.2

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -35,36 +36,38 @@ public class ShoppingListActivity extends AppCompatActivity {
     private EditText edit_item;
 
     private void writeItemList () {
-        /*
-
-        fos.write(string.getBytes());
-        fos.close();
-
-        Patatas; false
-        Papel WC; true
-        1; true
-        2; false
-        3; false
-
-         */
-
         try {
             FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
             for (int i = 0; i < itemList.size(); i++){
                 ShoppingItem it = itemList.get(i);
                 String line = String.format("%s; %b\n", it.getText(), it.isChecked());
                 fos.write(line.getBytes());
-
             }
+            fos.close();
 
         } catch (FileNotFoundException e) {
             Log.e("markandirt", "writeItemist: FileNotFoundException");
             Toast.makeText(this, R.string.cannot_write, Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
+            Log.e("markandirt", "writeItemist: FileNotFoundException");
+            Toast.makeText(this, R.string.cannot_write, Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private void readItemList() {
+        itemList = new ArrayList<>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+        } catch (FileNotFoundException e) {
 
         }
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        writeItemList();
     }
 
     @Override
